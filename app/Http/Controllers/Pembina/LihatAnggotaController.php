2c95b19c1e2f317a28a182b5df128069a18e0329
Rafi Prabowo <iydrafiprabowo@gmail.com>
2024-12-18 14:46:44 +0700

    fix lihat peserta

--- app/Http/Controllers/Pembina/LihatAnggotaController.php
+++ app/Http/Controllers/Pembina/LihatAnggotaController.php
@@ -8,14 +8,20 @@ use App\Http\Controllers\Controller;
 class LihatAnggotaController extends Controller
 {
     public function index()
-    {
-        // Mengambil data peserta dengan relasi ke regu_pembinas dan mata_lombas
-        $pembina = \App\Models\Pembina::with('regu.peserta')->find(auth()->user()->pembina->id);
+{
+    // Pastikan user memiliki pembina
+    $user = auth()->user();
+    $pembinaId = optional($user->pembina)->id;
 
-        if(!$pembina){
-            $pembina = new \App\Models\Pembina();
-        }
-        
-        return view('pembina.lihat-anggota', compact('pembina'));
+    // Mengambil data peserta dengan relasi ke regu_pembinas dan mata_lombas
+    $pembina = \App\Models\Pembina::with('regu.peserta')->find($pembinaId);
+
+    if (!$pembina) {
+        // Jika pembina tidak ditemukan, buat objek kosong
+        $pembina = new \App\Models\Pembina();
     }
+
+    return view('pembina.lihat-anggota', compact('pembina'));
+}
+
 }
